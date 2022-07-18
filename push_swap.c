e0fdc5cec715236b4e87608f406e3f845eda3abd
jaeyjeon <n8720432@naver.com>
2022-07-18 22:14:30 +0900

    firstsetting3

--- push_swap.c
+++ push_swap.c
@@ -6,14 +6,39 @@
 /*   By: jaeyjeon <@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/18 21:45:13 by jaeyjeon          #+#    #+#             */
-/*   Updated: 2022/07/18 21:59:53 by jaeyjeon         ###   ########.fr       */
+/*   Updated: 2022/07/18 22:13:49 by jaeyjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(void)
+int	check_argv(char *str)
 {
-	ft_printf("ABCDEFG!!!\n");
+	while (*str)
+	{
+		if (*str < '0' || *str > '9' )
+			return (1);
+		str++;
+	}
 	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	if (argc == 1)
+	{
+		ft_printf("nothing turned in!!\n");
+		return (0);
+	}
+	while (i != argc)
+	{
+		ft_printf("check %s\n", argv[i]);
+		if (check_argv(argv[i]))
+			return (0);
+		i++;
+	}
+	ft_printf("good\n");
 }
